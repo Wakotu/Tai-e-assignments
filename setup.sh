1be1b63524517ef8e590a7f23643bb9851851e66
Wakotu <442444286@qq.com>
2024-01-02 15:16:13 +0800

    adjust setup script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # WARNING: should be invoked only once with caution
 PA="$1"
 
-git checkout -B "$PA"
+git checkout -b "$PA"
 
 find . -maxdepth 1 -type f -name "*.sh" ! -name "setup.sh" -exec cp {} "${PA}/tai-e/" \;
+cd "${PA}/tai-e" || exit
