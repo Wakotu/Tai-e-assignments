3114f81e3c400d0b8a98919cac1383b049747cab
Wakotu <442444286@qq.com>
2024-01-07 13:56:02 +0800

    init branch

---
+++ A4/tai-e/getsubmit.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+dir='submit'
+
+if [[ -d "$dir" ]]; then
+	rm -r "$dir"
+fi
+
+mkdir "$dir"
+
+files=()
+
+basenames=('ConstantPropagation.java' 'Solver.java' 'WorkListSolver.java')
+
+for name in "${basenames[@]}"; do
+	files+=($(find . -name "$name"))
+done
+
+# echo "${files[@]}"
+
+# copy files
+for i in "${!files[@]}"; do
+	cp "${files[$i]}" "$dir"
+done
+
+parent_dir=$(dirname "$(pwd)")
+parent_base=$(basename "$parent_dir")
+cd "$dir" || exit
+zip "${parent_base}.zip" *
+
+PA_DIR="${HOME}/Documents/pa/"
+mkdir -p "$PA_DIR"
+cp -f "${parent_base}.zip" "$PA_DIR"
---
+++ A4/tai-e/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./gradlew run --args="-cp $1 -m $2"
