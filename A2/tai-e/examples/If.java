492c3a207374aad79141105502ed448afc1b4a34
Wakotu <442444286@qq.com>
2024-01-07 13:53:52 +0800

    add some example

---
+++ A2/tai-e/examples/If.java
@@ -0,0 +1,13 @@
+public class If {
+  public static void main(String[] args) {
+    int x = 1;
+    int y;
+    if (x < 0) {
+      y = 10;
+    } else if (x > 10) {
+      y = 20;
+    } else {
+      y = 30;
+    }
+  }
+}
