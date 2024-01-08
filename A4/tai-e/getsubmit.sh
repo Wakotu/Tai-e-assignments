37b354c8f2f1a5c344aef5bb5b1953915ece93f9
Wakotu <442444286@qq.com>
2024-01-08 22:18:59 +0800

    adjust submit script

--- A4/tai-e/getsubmit.sh
+++ A4/tai-e/getsubmit.sh
@@ -10,7 +10,7 @@ mkdir "$dir"
 
 files=()
 
-basenames=('ConstantPropagation.java' 'Solver.java' 'WorkListSolver.java')
+basenames=('InterConstantPropagation.java' 'InterSolver.java' 'CHABuilder.java')
 
 for name in "${basenames[@]}"; do
 	files+=($(find . -name "$name"))
